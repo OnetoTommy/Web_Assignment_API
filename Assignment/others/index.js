99f2b6967630bbdbe3dfcbc510139260e02723f3
OnetoTommy <xingguangc09@gmail.com>
2025-04-02 12:15:03 -0700

    Assigment

--- Assignment/others/index.js
+++
@@ -1,9 +0,0 @@
-const a = 'Northeastern University'
-
-fetch(`http://universities.hipolabs.com/search?name=${a}`)
-  .then((response) => response.json())
-  .then(data => {
-    console.log(data);
-  })
-
-  .catch(error => console.error(error))
